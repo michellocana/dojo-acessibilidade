e50d83fcbe0970da968c61485ffa24fd149e1088
Michell Ocaña <ocana.1997@hotmail.com>
2019-09-28 17:07:54 -0300

    feat: alt basic example

---
+++ src/components/AltExample.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AltExample = () => {
+  return (
+    <img
+      width={560}
+      src='./images/walking-on-rock.jpg'
+      alt='Pessoa em cima de uma formação rochosa.'
+    />
+  )
+}
+
+export default AltExample
