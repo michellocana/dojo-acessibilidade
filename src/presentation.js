f9380e48ce456cafdc6fcd58f055a99a218196f1
Michell Ocaña <ocana.1997@hotmail.com>
2019-09-29 23:36:39 -0300

    feat: contact info and references

--- src/presentation.js
+++ src/presentation.js
@@ -1629,6 +1629,320 @@ if(window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
           </ListItem>
         </List>
       </Slide>
+
+      <Slide>
+        <Heading size={1} caps textSize={56}>
+          Concluindo
+        </Heading>
+
+        <Notes>
+          Preciso seguir todas as dicas?
+          <br />
+          <br />
+          Acessibilidade = prima-irmã da UX
+          <br />
+          <br />A Web foi feita pra ser acessível desde o começo
+        </Notes>
+      </Slide>
+
+      <Slide>
+        <BlockQuote>
+          <Quote textSize={50} textColor='secondary' style={{ borderColor: 'white' }}>
+            The power of the Web is in its universality. Access by everyone regardless of disability
+            is an essential aspect
+          </Quote>
+          <Cite margin={20}>Tim Berners-Lee</Cite>
+        </BlockQuote>
+      </Slide>
+
+      <Slide>
+        <Layout style={{ justifyContent: 'center' }}>
+          <Fit>
+            <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
+              Obrigado!
+            </Heading>
+
+            <Text
+              textColor='secondary'
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 5
+              }}
+            >
+              <img width={40} style={{ marginRight: 10 }} src='./images/slack.svg' alt='Slack' />
+              michell.santo
+            </Text>
+
+            <Text
+              textColor='secondary'
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 5
+              }}
+            >
+              <img width={40} style={{ marginRight: 10 }} src='./images/mail.svg' alt='E-mail' />
+              michell.santo@cwi.com.br
+            </Text>
+          </Fit>
+        </Layout>
+      </Slide>
+
+      <Slide>
+        <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
+          Referências
+        </Heading>
+
+        <List>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://medium.com/@superplane39/i-spent-three-months-as-a-keyboard-only-user-heres-what-i-learned-d5d19ccbef35'
+            >
+              I spent three months as a keyboard-only user. Here’s what I learned.
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='http://adrianroselli.com/2019/02/uncanny-a11y.html'
+            >
+              Uncanny A11y
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link target='_blank' textColor='tertiary' href='https://webaccessibility.guide/'>
+              Web Accessibility Guide
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link target='_blank' textColor='tertiary' href='http://shouldiuseacarousel.com/'>
+              Should I Use A Carousel?
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link target='_blank' textColor='tertiary' href='https://gaia.wiki.br/'>
+              GAIA - Guia de Acessibilidade de Interfaces Web focado em Autismo
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.slideshare.net/mobile/talitapagani/guia-de-acessibilidade-de-interfaces-web-focado-em-autismo'
+            >
+              Apresentação do GAIA
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://twitter.com/TatianaTMac/status/1127584204613505025'
+            >
+              Thread sobre HTML semântico
+            </Link>
+          </ListItem>
+        </List>
+      </Slide>
+
+      <Slide>
+        <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
+          Referências
+        </Heading>
+
+        <List>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://news.microsoft.com/stories/explanimators/inclusive-design'
+            >
+              The animated guide to inclusive design
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://twitter.com/designernatan/status/1129009869074423813'
+            >
+              Filtros de cores do Windows
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.sarasoueidan.com/blog/accessible-icon-buttons/'
+            >
+              Accessible Icon Buttons
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://twitter.com/SaraSoueidan/status/1135249149958479872'
+            >
+              Thread sobre navegação pelo teclado
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.matuzo.at/blog/building-the-most-inaccessible-site-possible-with-a-perfect-lighthouse-score/'
+            >
+              Building the most inaccessible site possible with a perfect Lighthouse score
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link target='_blank' textColor='tertiary' href='http://designinclusivo.com/'>
+              Princípios do design inclusivo
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://24ways.org/2017/accessibility-through-semantic-html/'
+            >
+              Accessibility Through Semantic HTML
+            </Link>
+          </ListItem>
+        </List>
+      </Slide>
+
+      <Slide>
+        <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
+          Referências
+        </Heading>
+
+        <List>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.uxpin.com/studio/blog/8-website-accessibility-best-practices-to-improve-ux/'
+            >
+              8 Website Accessibility Best Practices to Improve UX
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://developers.google.com/web/fundamentals/accessibility/'
+            >
+              Accessibility | Web Fundamentals
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://medium.com/@matuzo/writing-css-with-accessibility-in-mind-8514a0007939'
+            >
+              Writing CSS with Accessibility in Mind
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://adamsilver.io/articles/the-problem-with-tooltips-and-what-to-do-instead/'
+            >
+              The problem with tooltips and what to do instead
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.mightybytes.com/blog/how-many-users-with-disabilities-on-site/'
+            >
+              How Many People With Disabilities Use My Website?
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://webaccess.berkeley.edu/ask-pecan/click-here'
+            >
+              What’s wrong with using "Click Here" links?
+            </Link>
+          </ListItem>
+        </List>
+      </Slide>
+
+      <Slide>
+        <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
+          Referências
+        </Heading>
+
+        <List>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://medium.muz.li/the-science-of-color-contrast-an-expert-designers-guide-33e84c41d156'
+            >
+              The Science of Color Contrast — An Expert Designer’s Guide
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.deque.com/blog/current-design-trends-affect-web-accessibility/'
+            >
+              How Current Design Trends Affect Web Accessibility
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link target='_blank' textColor='tertiary' href='https://webaim.org/articles/seizure/'>
+              Seizure Disorders
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://medium.com/@zacdicko/size-matters-accessibility-and-touch-targets-56e942adc0cc'
+            >
+              Size matters! Accessibility and Touch Targets
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://medium.com/codeandco/ux-making-experiences-feel-natural-with-feedback-loops-ce4eb333f99c'
+            >
+              UX: Creating natural experiences with feedback loops
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://uxdesign.cc/alternatives-to-placeholder-text-13f430abc56f'
+            >
+              Alternatives to Placeholder Text
+            </Link>
+          </ListItem>
+
+          {/* TODO alphabetical order references */}
+        </List>
+      </Slide>
     </Deck>
   )
 }
