08c95bc80d81e97d507d93e202e69c5454b98ebb
Michell Ocana do Espirito Santo <michell.santo@cwi.com.br>
2019-10-01 14:32:52 -0300

    feat: sorting references alphabetically

--- src/presentation.js
+++ src/presentation.js
@@ -69,7 +69,7 @@ const Presentation = () => {
       progress='bar'
     >
       <Slide>
-       <IntroBackground />
+        <IntroBackground />
 
         <Heading size={1} fit caps>
           Acessibilidade Web
@@ -1689,6 +1689,127 @@ if(window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
         <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
           Referências
         </Heading>
+        <List>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.uxpin.com/studio/blog/8-website-accessibility-best-practices-to-improve-ux/'
+            >
+              8 Website Accessibility Best Practices to Improve UX
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://24ways.org/2017/accessibility-through-semantic-html/'
+            >
+              Accessibility Through Semantic HTML
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://developers.google.com/web/fundamentals/accessibility/'
+            >
+              Accessibility | Web Fundamentals
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.sarasoueidan.com/blog/accessible-icon-buttons/'
+            >
+              Accessible Icon Buttons
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://uxdesign.cc/alternatives-to-placeholder-text-13f430abc56f'
+            >
+              Alternatives to Placeholder Text
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.slideshare.net/mobile/talitapagani/guia-de-acessibilidade-de-interfaces-web-focado-em-autismo'
+            >
+              Apresentação do GAIA
+            </Link>
+          </ListItem>
+        </List>
+      </Slide>
+
+      <Slide>
+        <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
+          Referências
+        </Heading>
+
+        <List>
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.matuzo.at/blog/building-the-most-inaccessible-site-possible-with-a-perfect-lighthouse-score/'
+            >
+              Building the most inaccessible site possible with a perfect Lighthouse score
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://twitter.com/designernatan/status/1129009869074423813'
+            >
+              Filtros de cores do Windows
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link target='_blank' textColor='tertiary' href='https://gaia.wiki.br/'>
+              GAIA - Guia de Acessibilidade de Interfaces Web focado em Autismo
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.deque.com/blog/current-design-trends-affect-web-accessibility/'
+            >
+              How Current Design Trends Affect Web Accessibility
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://www.mightybytes.com/blog/how-many-users-with-disabilities-on-site/'
+            >
+              How Many People With Disabilities Use My Website?
+            </Link>
+          </ListItem>
+        </List>
+      </Slide>
+
+      <Slide>
+        <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
+          Referências
+        </Heading>
 
         <List>
           <ListItem>
@@ -1701,19 +1822,17 @@ if(window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
             </Link>
           </ListItem>
           <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='http://adrianroselli.com/2019/02/uncanny-a11y.html'
-            >
-              Uncanny A11y
+            <Link target='_blank' textColor='tertiary' href='http://designinclusivo.com/'>
+              Princípios do design inclusivo
             </Link>
           </ListItem>
+
           <ListItem>
-            <Link target='_blank' textColor='tertiary' href='https://webaccessibility.guide/'>
-              Web Accessibility Guide
+            <Link target='_blank' textColor='tertiary' href='https://webaim.org/articles/seizure/'>
+              Seizure Disorders
             </Link>
           </ListItem>
+
           <ListItem>
             <Link target='_blank' textColor='tertiary' href='http://shouldiuseacarousel.com/'>
               Should I Use A Carousel?
@@ -1721,26 +1840,22 @@ if(window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
           </ListItem>
 
           <ListItem>
-            <Link target='_blank' textColor='tertiary' href='https://gaia.wiki.br/'>
-              GAIA - Guia de Acessibilidade de Interfaces Web focado em Autismo
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://medium.com/@zacdicko/size-matters-accessibility-and-touch-targets-56e942adc0cc'
+            >
+              Size matters! Accessibility and Touch Targets
             </Link>
           </ListItem>
+
           <ListItem>
             <Link
               target='_blank'
               textColor='tertiary'
-              href='https://www.slideshare.net/mobile/talitapagani/guia-de-acessibilidade-de-interfaces-web-focado-em-autismo'
+              href='https://medium.muz.li/the-science-of-color-contrast-an-expert-designers-guide-33e84c41d156'
             >
-              Apresentação do GAIA
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://twitter.com/TatianaTMac/status/1127584204613505025'
-            >
-              Thread sobre HTML semântico
+              The Science of Color Contrast — An Expert Designer’s Guide
             </Link>
           </ListItem>
         </List>
@@ -1761,92 +1876,7 @@ if(window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
               The animated guide to inclusive design
             </Link>
           </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://twitter.com/designernatan/status/1129009869074423813'
-            >
-              Filtros de cores do Windows
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://www.sarasoueidan.com/blog/accessible-icon-buttons/'
-            >
-              Accessible Icon Buttons
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://twitter.com/SaraSoueidan/status/1135249149958479872'
-            >
-              Thread sobre navegação pelo teclado
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://www.matuzo.at/blog/building-the-most-inaccessible-site-possible-with-a-perfect-lighthouse-score/'
-            >
-              Building the most inaccessible site possible with a perfect Lighthouse score
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link target='_blank' textColor='tertiary' href='http://designinclusivo.com/'>
-              Princípios do design inclusivo
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://24ways.org/2017/accessibility-through-semantic-html/'
-            >
-              Accessibility Through Semantic HTML
-            </Link>
-          </ListItem>
-        </List>
-      </Slide>
 
-      <Slide>
-        <Heading size={1} caps textSize={56} style={{ paddingBottom: 20 }}>
-          Referências
-        </Heading>
-
-        <List>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://www.uxpin.com/studio/blog/8-website-accessibility-best-practices-to-improve-ux/'
-            >
-              8 Website Accessibility Best Practices to Improve UX
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://developers.google.com/web/fundamentals/accessibility/'
-            >
-              Accessibility | Web Fundamentals
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://medium.com/@matuzo/writing-css-with-accessibility-in-mind-8514a0007939'
-            >
-              Writing CSS with Accessibility in Mind
-            </Link>
-          </ListItem>
           <ListItem>
             <Link
               target='_blank'
@@ -1856,22 +1886,44 @@ if(window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
               The problem with tooltips and what to do instead
             </Link>
           </ListItem>
+
           <ListItem>
             <Link
               target='_blank'
               textColor='tertiary'
-              href='https://www.mightybytes.com/blog/how-many-users-with-disabilities-on-site/'
+              href='https://twitter.com/TatianaTMac/status/1127584204613505025'
             >
-              How Many People With Disabilities Use My Website?
+              Thread sobre HTML semântico
             </Link>
           </ListItem>
+
           <ListItem>
             <Link
               target='_blank'
               textColor='tertiary'
-              href='https://webaccess.berkeley.edu/ask-pecan/click-here'
+              href='https://twitter.com/SaraSoueidan/status/1135249149958479872'
             >
-              What’s wrong with using "Click Here" links?
+              Thread sobre navegação pelo teclado
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://medium.com/codeandco/ux-making-experiences-feel-natural-with-feedback-loops-ce4eb333f99c'
+            >
+              UX: Creating natural experiences with feedback loops
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='http://adrianroselli.com/2019/02/uncanny-a11y.html'
+            >
+              Uncanny A11y
             </Link>
           </ListItem>
         </List>
@@ -1884,57 +1936,30 @@ if(window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
 
         <List>
           <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://medium.muz.li/the-science-of-color-contrast-an-expert-designers-guide-33e84c41d156'
-            >
-              The Science of Color Contrast — An Expert Designer’s Guide
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://www.deque.com/blog/current-design-trends-affect-web-accessibility/'
-            >
-              How Current Design Trends Affect Web Accessibility
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link target='_blank' textColor='tertiary' href='https://webaim.org/articles/seizure/'>
-              Seizure Disorders
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://medium.com/@zacdicko/size-matters-accessibility-and-touch-targets-56e942adc0cc'
-            >
-              Size matters! Accessibility and Touch Targets
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://medium.com/codeandco/ux-making-experiences-feel-natural-with-feedback-loops-ce4eb333f99c'
-            >
-              UX: Creating natural experiences with feedback loops
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              target='_blank'
-              textColor='tertiary'
-              href='https://uxdesign.cc/alternatives-to-placeholder-text-13f430abc56f'
-            >
-              Alternatives to Placeholder Text
+            <Link target='_blank' textColor='tertiary' href='https://webaccessibility.guide/'>
+              Web Accessibility Guide
             </Link>
           </ListItem>
 
-          {/* TODO alphabetical order references */}
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://webaccess.berkeley.edu/ask-pecan/click-here'
+            >
+              What’s wrong with using "Click Here" links?
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link
+              target='_blank'
+              textColor='tertiary'
+              href='https://medium.com/@matuzo/writing-css-with-accessibility-in-mind-8514a0007939'
+            >
+              Writing CSS with Accessibility in Mind
+            </Link>
+          </ListItem>
         </List>
       </Slide>
     </Deck>
