cb8efdabc035cc4633747e371de66f91bb2e8702
Michell Ocana do Espirito Santo <michell.santo@cwi.com.br>
2019-10-01 16:00:55 -0300

    chore: rebuilding static version with homepage url

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.583a52a9534aa843c6bb9bb262fb4102.js"
+  "/dojo-acessibilidade/presentation/precache-manifest.972811e4ba07bf417d5542f4ba51e193.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dojo-acessibilidade/presentation/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
