4659b75375421ab972eb5ea7dd62f4d6daea9b31
Michell Ocaña <ocana.1997@hotmail.com>
2019-11-10 13:32:50 -0300

    feat: dont disable zoom content adjusts

--- src/slides/DontDisableZoom.js
+++ src/slides/DontDisableZoom.js
@@ -1,29 +1,108 @@
 import React from 'react'
 import { Slide, Heading, Text, Notes } from 'spectacle'
 
-import MetaViewport from '../components/MetaViewport'
+import TextHighlight from '../components/TextHighlight'
+
+// import MetaViewport from '../components/MetaViewport'
+
+const ICON_WIDTH = 37
 
 function DontDisableZoom() {
-  return (
+  function renderText(text, icon = './images/plus-icon.svg') {
+    return (
+      <Text textColor='secondary' textAlign='left' style={{ paddingBottom: 10 }}>
+        <img
+          alt=''
+          src={icon}
+          width={ICON_WIDTH}
+          style={{
+            display: 'inline-block',
+            verticalAlign: 'middle',
+            marginRight: 10
+          }}
+        />
+        {text}
+      </Text>
+    )
+  }
+
+  function renderPlusText(text) {
+    return renderText(text, './images/plus-icon.svg')
+  }
+
+  function renderLessText(text) {
+    return renderText(text, './images/less-icon.svg')
+  }
+
+  return [
     <Slide>
-      <Heading size={1} fit caps>
-        Boas Práticas - Não desabilite o zoom da página
+      <Heading size={1} textSize={55} caps>
+        Não tire o poder do usuário decidir o tamanho das coisas na tela (zoom)
       </Heading>
 
-      <Text fit textColor='secondary' textSize={21}>
-        Não tire o poder do usuário decidir o tamanho das coisas na tela
-      </Text>
+      {/* <MetaViewport /> */}
+    </Slide>,
 
-      <MetaViewport />
+    <Slide>
+      <Heading size={1} fit caps>
+        Tudo começou quando
+        <br />o design responsivo foi adotado
+      </Heading>
 
       <Notes>
-        Desde que o RWD (design responsivo) começou a ser adotado, ficou comum o uso dessa meta tag
+        Ficou comum a prática de <TextHighlight>desativar o zoom</TextHighlight>
         <br />
         <br />
-        Mas ela desabilita o zoom, então é melhor usar essa segunda versão dela
+        Afinal, <TextHighlight>tudo está do tamanho desejado para todo mundo</TextHighlight>, certo?
+        Errado.
       </Notes>
+    </Slide>,
+
+    <Slide>
+      <Heading size={1} fit caps>
+        Escolhas que podemos
+        <br />
+        fazer em relação ao zoom
+      </Heading>
+
+      <Notes>Temos 3 escolhas que podemos fazer em relação à isso</Notes>
+    </Slide>,
+
+    <Slide>
+      <Heading size={1} fit caps style={{ paddingBottom: 10 }}>
+        Usar fontes extra-grandes para tudo
+      </Heading>
+
+      {renderPlusText('Beneficia usuários mais velhos / com dificuldades visuais')}
+      {renderLessText('Prejudica a estética do site')}
+      {renderLessText('Limita a quantidade de conteúdo na tela')}
+
+      <Notes>
+        Essa é uma <TextHighlight>estratégia inclusiva</TextHighlight>, mas talvez inclusiva de mais
+      </Notes>
+    </Slide>,
+
+    <Slide>
+      <Heading size={1} fit caps style={{ paddingBottom: 10 }}>
+        Fazer design só para a maioria
+      </Heading>
+
+      {renderLessText('Aceitar que meu site não é inclusivo')}
+
+      <Notes>Estratégia cômoda e exclusiva</Notes>
+    </Slide>,
+
+    <Slide>
+      <Heading size={1} fit caps style={{ paddingBottom: 10 }}>
+        Não desabilitar o zoom
+      </Heading>
+
+      {renderPlusText('Liberdade pro usuário')}
+      {renderLessText('Ao custo de alguns gestos de pinch')}
+
+      <Notes>Estratégia inclusiva</Notes>
     </Slide>
-  )
+  ]
 }
 
 export default DontDisableZoom
