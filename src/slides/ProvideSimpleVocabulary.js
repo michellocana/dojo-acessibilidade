8e5e43d01e7c0f91fe1783fc921b42e8a1f5a575
Michell Ocaña <ocana.1997@hotmail.com>
2019-11-11 22:40:37 -0300

    feat: adjusting simple vocabulary slide

--- src/slides/ProvideSimpleVocabulary.js
+++ src/slides/ProvideSimpleVocabulary.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import { Slide, Heading, List, ListItem, S } from 'spectacle'
+import { Slide, Heading, Notes, List, ListItem } from 'spectacle'
+
+import TextHighlight from '../components/TextHighlight'
 
 function ProvideSimpleVocabulary() {
   return (
     <Slide>
       <Heading size={1} fit caps>
-        Forneça um vocabulário simples...
+        Forneça um vocabulário simples,
         <br />
-        sempre que possível evite:
+        sempre que possível, evite o uso de:
       </Heading>
 
       <List>
-        <ListItem>Metáforas: Pode confudir usuários com autismo</ListItem>
-        <ListItem>
-          Abreviações/acrônimos: Se usar, forneça o significado da sigla ou da abreviação (usando a
-          tag
-          <S type='bold' textColor='tertiary'>
-            {' <abbr>'}
-          </S>{' '}
-          por exemplo)
-        </ListItem>
+        <ListItem>Metáforas</ListItem>
+        <ListItem>Coloquialismo</ListItem>
+        <ListItem>Expressões não literais</ListItem>
+        <ListItem>Abreviações/acrônimos</ListItem>
       </List>
+
+      <Notes>
+        Usar uma linguagem que <TextHighlight>foge do padrão</TextHighlight> pode
+        confundir/estressar o usuário com <TextHighlight>autismo</TextHighlight>
+        <br />
+        <br />
+        Se usar <TextHighlight>abreviações</TextHighlight> ou{' '}
+        <TextHighlight>acrônimos</TextHighlight> forneça o significado da sigla ou da abreviação
+      </Notes>
     </Slide>
   )
 }
