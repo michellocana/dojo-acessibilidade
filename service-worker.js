821f8eb223559439e5adcfd51ebf706a04a37763
Michell Ocana do Espirito Santo <michell.santo@cwi.com.br>
2019-11-14 09:20:40 -0300

    feat: updating static version

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dojo-acessibilidade/precache-manifest.51fd1ac92747f5a27ff1ca1fff760dcf.js"
+  "/dojo-acessibilidade/precache-manifest.c84fd0177a3419f703efcf030a7c0bfb.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/dojo-acessibilidade/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
